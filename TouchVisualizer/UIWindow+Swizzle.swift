3d8b9f32aebdcb8045f886d462e22c566f1fb811
Shou Wei <sw0906@gmail.com>
2018-04-13 17:57:45 +0800

    update swizzle

--- TouchVisualizer/UIWindow+Swizzle.swift
+++ TouchVisualizer/UIWindow+Swizzle.swift
@@ -7,8 +7,6 @@ import UIKit
 
 fileprivate var isSwizzled = false
 
-fileprivate var isSwizzledPush = false
-
 @available(iOS 8.0, *)
 extension UIWindow {
 	
@@ -19,29 +17,26 @@ extension UIWindow {
 		
 		let sendEvent = class_getInstanceMethod(object_getClass(self), #selector(UIApplication.sendEvent(_:)))
 		let swizzledSendEvent = class_getInstanceMethod(object_getClass(self), #selector(UIWindow.swizzledSendEvent(_:)))
-        method_exchangeImplementations(sendEvent!, swizzledSendEvent!)        
+        method_exchangeImplementations(sendEvent!, swizzledSendEvent!)
+        swizzleNav()
         isSwizzled = true
+    }
+    
+    public func swizzleNav() {
+        let pushEvent = class_getInstanceMethod(UINavigationController.classForCoder(), #selector(UINavigationController.pushViewController(_:animated:)))
+        let swizzledPushEvent = class_getInstanceMethod(UINavigationController.classForCoder(), #selector(UINavigationController.swizzledPushViewController(_:animated:)))
+        method_exchangeImplementations(pushEvent!, swizzledPushEvent!)
     }
     
     @objc public func swizzledSendEvent(_ event: UIEvent) {
         Visualizer.sharedInstance.handleEvent(event)
         swizzledSendEvent(event)
     }
+    
 }
 
 extension UINavigationController {
-    
-    public func swizzle() {
-        if (isSwizzledPush) {
-            return
-        }
-        
-        let pushEvent = class_getInstanceMethod(object_getClass(self), #selector(UINavigationController.pushViewController(_:animated:)))
-        let swizzledPushEvent = class_getInstanceMethod(object_getClass(self), #selector(UINavigationController.swizzledPushViewController(_:animated:)))
-        method_exchangeImplementations(pushEvent!, swizzledPushEvent!)
-        isSwizzledPush = true
-    }
-    
+
     @objc public func swizzledPushViewController(_ viewController: UIViewController, animated: Bool) {
         Visualizer.sharedInstance.removeAllTouchViews()
         swizzledPushViewController(viewController, animated: animated)
