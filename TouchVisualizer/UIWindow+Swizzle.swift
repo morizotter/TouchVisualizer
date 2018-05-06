a4e0867858ae0900b4fd13621fb8fac28614586c
Naoki Morita <namorit@gmail.com>
2018-05-06 14:37:22 +0900

    Clean up

--- TouchVisualizer/UIWindow+Swizzle.swift
+++ TouchVisualizer/UIWindow+Swizzle.swift
@@ -11,32 +11,43 @@ fileprivate var isSwizzled = false
 extension UIWindow {
 	
 	public func swizzle() {
-		if (isSwizzled) {
-			return
-		}
-		
-		let sendEvent = class_getInstanceMethod(object_getClass(self), #selector(UIApplication.sendEvent(_:)))
-		let swizzledSendEvent = class_getInstanceMethod(object_getClass(self), #selector(UIWindow.swizzledSendEvent(_:)))
+        guard isSwizzled == false else {
+            return
+        }
+
+		let sendEvent = class_getInstanceMethod(
+            object_getClass(self), 
+            #selector(UIApplication.sendEvent(_:))
+        )
+		let swizzledSendEvent = class_getInstanceMethod(
+            object_getClass(self), 
+            #selector(UIWindow.swizzledSendEvent(_:))
+        )
         method_exchangeImplementations(sendEvent!, swizzledSendEvent!)
-        swizzleNav()
+        
+        let pushEvent = class_getInstanceMethod(
+            UINavigationController.classForCoder(), 
+            #selector(UINavigationController.pushViewController(_:animated:))
+        )
+        let swizzledPushEvent = class_getInstanceMethod(
+            UINavigationController.classForCoder(), 
+            #selector(UINavigationController.swizzledPushViewController(_:animated:))
+        )
+        method_exchangeImplementations(pushEvent!, swizzledPushEvent!)
+        
         isSwizzled = true
     }
-    
-    public func swizzleNav() {
-        let pushEvent = class_getInstanceMethod(UINavigationController.classForCoder(), #selector(UINavigationController.pushViewController(_:animated:)))
-        let swizzledPushEvent = class_getInstanceMethod(UINavigationController.classForCoder(), #selector(UINavigationController.swizzledPushViewController(_:animated:)))
-        method_exchangeImplementations(pushEvent!, swizzledPushEvent!)
-    }
-    
+}
+
+// MARK: - Swizzle
+extension UIWindow {
     @objc public func swizzledSendEvent(_ event: UIEvent) {
         Visualizer.sharedInstance.handleEvent(event)
         swizzledSendEvent(event)
     }
-    
 }
 
 extension UINavigationController {
-
     @objc public func swizzledPushViewController(_ viewController: UIViewController, animated: Bool) {
         Visualizer.sharedInstance.removeAllTouchViews()
         swizzledPushViewController(viewController, animated: animated)
