78a5a03f3cfc688de58a0636bd42d4712abb9f76
Bruno Guidolim <bguidolim@gmail.com>
2021-01-18 14:45:48 +0100

    Added support SwiftPM

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TouchVisualizer",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(
+            name: "TouchVisualizer",
+            targets: ["TouchVisualizer"]),
+    ],
+    targets: [
+        .target(
+            name: "TouchVisualizer",
+            path: "TouchVisualizer",
+            exclude: ["Info.plist"]
+        )
+    ]
+)
